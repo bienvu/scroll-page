f96ae45ec31188add897c58a093c8f197c60485b
BienVu <bien.vu@ffwagency.com>
2018-05-30 01:07:11 +0700

    Update code

--- demo3/js/index.js
+++ demo3/js/index.js
@@ -1,21 +1,49 @@
-var listElm = document.querySelector('#infinite-list');
+
+$(document).ready(function() {
+  loadMore();
+});
+
+var listElm = $('#infinite-list');
 
 // Add 20 items.
 var nextItem = 1;
 var loadMore = function() {
-  for (var i = 0; i < 60; i++) {
-    var item = document.createElement('li');
-    item.innerText = 'Item ' + nextItem++;
-    listElm.appendChild(item);
+  // listElm.find('.item').clone().appendTo(listElm);
+  for (var i = 0; i < 1; i++) {
+    // var item = document.createElement('li');
+    var html='<div> <a href="#"> <img class="pic" src="https://bienvu.github.io/scroll-page/demo4/tipico.jpg" style="width: 100%; height: auto;" ></img></a><div>';
+    // item.append(html);
+    // // item.innerText = 'Item ' + nextItem++;
+    // // listElm.appendChild(item);
+    listElm.append(html);
   }
 }
 
+// var lastScrollTop = 0;
+//     $(window).on('scroll', function() {
+//         st = $(this).scrollTop();
+//         if(st < lastScrollTop) {
+//             console.log('up 1');
+//         }
+//         else {
+//             console.log('down 1');
+//         }
+//         lastScrollTop = st;
+//     });
+
 // Detect when scrolled to bottom.
-listElm.addEventListener('scroll', function() {
-  if (listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight) {
+listElm.on('scroll', function() {
+  var heightScroll = document.getElementById("infinite-list").scrollHeight,
+      heightScreen = listElm.height(),
+      topSpace = listElm.scrollTop();
+
+  // console.log(listElm.scrollTop());
+  // console.log(listElm.height());
+  // console.log(test);
+  // console.log(listElm.scrollHeight());
+  if (topSpace + heightScreen >= heightScroll) {
     loadMore();
   }
 });
 
 // Initially load some items.
-loadMore();
