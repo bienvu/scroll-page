642f571aab2645a24261af3797eb792361af29c8
BienVu <bien.vu@ffwagency.com>
2018-05-30 08:39:59 +0700

    update

--- demo5/js/main.js
+++ demo5/js/main.js
@@ -1,5 +1,6 @@
-var bgHeight = 6864; // pixel height of background image
-
+var bgHeight = 6000; // pixel height of background image
+console.log(bgHeight);
+console.log("ok");
 $(document).ready(function() {
     $('body').height( bgHeight + $(window).height() );
     $(window).scroll(function() {
