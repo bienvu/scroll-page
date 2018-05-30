7e01b254a99d55351ca3d98340448b4f2202bf1d
BienVu <bien.vu@ffwagency.com>
2018-05-30 07:05:57 +0700

    update

--- demo3/js/index.js
+++ demo3/js/index.js
@@ -21,6 +21,18 @@ var loadMore = function() {
   }
 }
 
+var loadPrev = function() {
+  // listElm.find('.item').clone().appendTo(listElm);
+  for (var i = 0; i < 1; i++) {
+    // var item = document.createElement('li');
+    var html='<div> <a href="#"> <img class="pic" src="https://bienvu.github.io/scroll-page/demo4/tipico.jpg" style="width: 100%; height: auto;" ></img></a><div>';
+    // item.append(html);
+    // // item.innerText = 'Item ' + nextItem++;
+    // // listElm.appendChild(item);
+    listElm.prepend(html);
+  }
+}
+
 // var lastScrollTop = 0;
 //     $(window).on('scroll', function() {
 //         st = $(this).scrollTop();
@@ -36,21 +48,43 @@ var loadMore = function() {
 // Detect when scrolled to bottom.
 // listElm.on('scroll touch', function() {
 // $(window).on('scroll', function() {
-$(window).on('scroll', function() {
+// $(window).on('scroll', function() {
+//   var heightScroll = document.getElementById("infinite-list").scrollHeight,
+//   // var heightScroll = document.getElementById("infinite-list").offset().top,
+//       heightScreen = $(window).height(),
+//       topSpace = $(window).scrollTop();
+//
+//   console.log(heightScroll);
+//   console.log(heightScreen);
+//   console.log(topSpace);
+//   // console.log(listElm.height());
+//   // console.log(test);
+//   // console.log(listElm.scrollHeight());
+//   if (topSpace + heightScreen >= heightScroll - 200) {
+//     loadMore();
+//   }
+// });
+
+var position = $(window).scrollTop();
+
+// should start at 0
+
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
   var heightScroll = document.getElementById("infinite-list").scrollHeight,
   // var heightScroll = document.getElementById("infinite-list").offset().top,
-      heightScreen = $(window).height(),
-      topSpace = $(window).scrollTop();
-
-  console.log(heightScroll);
-  console.log(heightScreen);
-  console.log(topSpace);
-  // console.log(listElm.height());
-  // console.log(test);
-  // console.log(listElm.scrollHeight());
-  if (topSpace + heightScreen >= heightScroll - 200) {
-    loadMore();
+      heightScreen = $(window).height();
+  if(scroll > position) {
+    if (scroll + heightScreen >= heightScroll - 200) {
+      loadMore();
+    }
+  } else {
+    if (scroll + heightScreen >= 200) {
+      loadPrev();
+    }
   }
+  position = scroll;
 });
- 
+
+
 // Initially load some items.
