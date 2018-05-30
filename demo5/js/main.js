41be21d4d42cfe2a167199df6634fcbaa965d7be
BienVu <bien.vu@ffwagency.com>
2018-05-30 09:39:40 +0700

    update

--- demo5/js/main.js
+++ demo5/js/main.js
@@ -1,5 +1,5 @@
 var bgHeight = 6000; // pixel height of background image
-console.log(bgHeight);
+console.log($(window).height());
 console.log("ok");
 $(document).ready(function() {
     $('body').height( bgHeight + $(window).height() );
@@ -7,7 +7,7 @@ $(document).ready(function() {
         if ( $(window).scrollTop() >= ($('body').height() - $(window).height()) - 200 ) {
             $(window).scrollTop(1);
         }
-        else if ( $(window).scrollTop() == 0 ) {
+        else if ( $(window).scrollTop() <= 10 ) {
             $(window).scrollTop($('body').height() - $(window).height() - 200);
         }
     });
